b08049a1126e055498ada30266b3bd6dccfca113
Murilo <dixmurilox@gmail.com>
2024-07-04 23:03:45 -0300

    feat: Include series data when fetching users in user route
    This commit modifies the user route to include the series data when fetching users from the database. By adding the `include` option with `series: true` in the `prisma.user.findMany` method, the series data will be included in the response. This enhancement improves the functionality of the user route by providing additional information about the users' associated series.

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -7,7 +7,11 @@ const router = Router();
 
 router.get("/", async (req, res) => {
     try {
-      const users = await prisma.user.findMany();
+      const users = await prisma.user.findMany({
+        include: {
+          series: true,
+        }
+      });
       res.status(200).json(users);
     } catch (error) {
       res.status(400).json(error);
