c4e5a5e8ad037d37a589cda616791ca9665af875
Murilo <dixmurilox@gmail.com>
2024-07-05 01:02:58 -0300

    feat: Add list_series function to retrieve and display series information
    This commit adds a new function called `list_series` to the code. This function makes a GET request to the `/series` endpoint and retrieves a list of series data. It then displays the series information, including the ID, name, number of seasons, genre, and streaming platform. This feature enhances the user interface by providing a way to view and browse the available series.

--- index.py
+++ index.py
@@ -2,8 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import requests
 from colorama import Fore, Back, Style
-# Rota para registrar usuario 
-# "http://localhost:3000/register"
 
 token = None
 
@@ -37,7 +35,7 @@ def login():
     url = "http://localhost:3000/login"
     response = requests.post(url, json={"email": email, "password": password})
     if response.status_code == 200:
-        print("Usuário logado com sucesso!")
+        print()
         token = response.json()["token"]
         return token
     else:
@@ -47,6 +45,27 @@ def login():
         print(Fore.RED, erro['erro'], Style.RESET_ALL)
         print("="*40)
         return None
+
+def list_series():
+    url = "http://localhost:3000/series"
+    response = requests.get(url)
+    series = response.json()
+    if response.status_code == 200:
+        print("=" * 40)
+        for serie in series:
+           print(f"{serie['id']} - {serie['name']} ({serie['seasons']} temporadas) - {serie['genre']} - {serie['streaming']}")
+        print("=" * 40)
+        
+    elif response.status_code == 404:
+        print("Nenhuma série encontrada!")
+    elif len(series) == 0:
+        print("Nenhuma série cadastrada!")
+    else:
+        print("Erro ao buscar séries!")
+        erro = response.json()
+        print("=" * 40)
+        print(erro)
+        print("=" * 40)
 
 def inclusion(token):
     print(token)
@@ -70,44 +89,144 @@ def inclusion(token):
         print(erro)
         print("=" * 40)
 
+def update_serie(token):
+    title("Atualização de Série", "-")
+    list_series()
+    id = input("ID da Série: ")
+    url = f"http://localhost:3000/series/{id}"
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        serie = response.json()
+        print("Série encontrada!")
+        print(serie["name"], serie["genre"], serie["streaming"], serie["seasons"])
+        option = input("Deseja atualizar a série? (s/n) ")
+        if option == "s":
+            serie["name"] = input("Nome da Série: ")
+            serie["seasons"] = int(input("Quantidade de Temporadas: "))
+            serie["genre"] = input("Gênero: ")
+            serie["streaming"] = input("Streaming: ")
+            response = requests.put(url, json=serie, headers=headers)
+            if response.status_code == 200:
+                print("Série atualizada com sucesso!")
+            else:
+                print("Erro ao atualizar série!")
+                erro = response.json()
+                print("=" * 40)
+                print(erro)
+                print("=" * 40)
+        else:
+            print("Operação cancelada!")
+    else:
+        print("Série não encontrada!")
+        erro = response.json()
+        print("=" * 40)
+        print(erro)
+        print("=" * 40)
 
-def graphic():
-    title("Gráfico de Séries", "-")
+def delete_serie(token):
+    title("Exclusão de Série", "-")
+    list_series()
+    id = int(input("Codigo da Série: "))
+
+    response = requests.delete(f"http://localhost:3000/series/{id}", headers = {"Authorization": f"Bearer {token}"})
+    if response.status_code == 200:
+        print("Série excluída com sucesso!")
+    else:
+        print("Erro ao excluir série!")
+        erro = response.json()
+        print("=" * 40)
+        print(erro)
+        print("=" * 40)
+
+def series_by_streaming():
+    title("Séries por Streaming", "-")
     url = "http://localhost:3000/series"
     response = requests.get(url)
     series = response.json()
-    genres = {}
-    for serie in series:
-        genre = serie["genre"]
-        if genre in genres:
-            genres[genre] += 1
-        else:
-            genres[genre] = 1
-    print(genres)
-    x = genres.keys()
-    y = genres.values()
-    plt.bar(x, y)
-    plt.show()
+
+    if response.status_code == 200 and len(series) > 0:
+        streaming_groups = {}
+        for serie in series:
+            streaming = serie["streaming"]
+            if streaming not in streaming_groups:
+                streaming_groups[streaming] = []
+            streaming_groups[streaming].append(serie)
+
+        for streaming, series_list in streaming_groups.items():
+            print(f"\n{streaming}:")
+            sorted_series = sorted(series_list, key=lambda x: x["seasons"], reverse=True)
+            for serie in sorted_series:
+                print(f"{serie['name']} ({serie['seasons']} temporadas)")
+    else:
+        print("Erro ao buscar séries ou nenhuma série encontrada!")
+        erro = response.json()
+        print("=" * 40)
+        print(erro)
+        print("=" * 40)
+
+def graphic():
+    title("Gráfico Comparativo", "-")
+    url = "http://localhost:3000/series"
+    response = requests.get(url)
+    series = response.json()
+
+    if response.status_code == 200 and len(series) > 0:
+        names = [serie["name"] for serie in series]
+        seasons = [serie["seasons"] for serie in series]
+
+        plt.figure(figsize=(10, 5))
+        plt.barh(names, seasons, color='skyblue')
+        plt.xlabel("Temporadas")
+        plt.ylabel("Séries")
+        plt.title("Número de Temporadas por Série")
+        plt.grid(axis='x', linestyle='--', alpha=0.7)
+        plt.show()
+    else:
+        print("Erro ao buscar séries ou nenhuma série encontrada!")
+        erro = response.json()
+        print("=" * 40)
+        print(erro)
+        print("=" * 40)
 
 #--------------------------- Main ---------------------------
+
 
 while True:
     if token is None:
         print("1 - Registrar")
         print("2 - Login")
-    if token is not None:
-        print("3 - Incluir Série")
-        print("4 - Gráfico")
-    option = input("Opção: ")
-    if option == "1":
-        register()
-    elif option == "2":
-        token = login()
-    elif option == "3" and token is not None:
-        inclusion(token)
-    elif option == "4" and token is not None:
-        graphic()
-    elif option == "0":
-        break
+        option = input("Opção: ")
+        
+        if option == "1":
+            register()
+        elif option == "2":
+            token = login()
+        else:
+            print("Opção inválida!")
     else:
-        print("Opção inválida!")
+        print("1 - Listar Séries")
+        print("2 - Incluir Série")
+        print("3 - Atualizar Série")
+        print("4 - Excluir Série")
+        print("5 - Gráfico")
+        print("6 - Series por Streaming")
+        print("0 - Sair")
+        option = input("Opção: ")
+        
+        if option == "1":
+            list_series()
+        elif option == "2":
+            inclusion(token)
+        elif option == "3":
+            update_serie(token)
+        elif option == "4":
+            delete_serie(token)
+        elif option == "5":
+            graphic()
+        elif option == "6":
+            series_by_streaming()
+        elif option == "0":
+            break
+        else:
+            print("Opção inválida!")
