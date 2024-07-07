ee34870b87ecc121463919fc64aa0a3f4d179a18
Murilo <dixmurilox@gmail.com>
2024-07-07 18:56:23 -0300

    feat: Refactor user route to use async/await syntax

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -4,33 +4,30 @@ import { Router } from "express";
 const prisma = new PrismaClient();
 const router = Router();
 
-
 router.get("/", async (req, res) => {
-    try {
-      const users = await prisma.user.findMany({
-        include: {
-          series: true,
-        }
-      });
-      res.status(200).json(users);
-    } catch (error) {
-      res.status(400).json(error);
-    }
-  });
+  try {
+    const users = await prisma.user.findMany({
+      include: {
+        series: true,
+      },
+    });
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
 router.delete("/:id", async (req, res) => {
-    const { id } = req.params;
-  
-    try {
-      await prisma.user.delete({
-        where: { id: Number(id) },
-      });
-      res.status(200).json("Usuario deletado com sucesso!");
-    } catch (error) {
-      res.status(400).json(error);
-    }
-    
-  });
+  const { id } = req.params;
 
+  try {
+    await prisma.user.delete({
+      where: { id: Number(id) },
+    });
+    res.status(200).json("Usuario deletado com sucesso!");
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
-export default router;  
+export default router;
